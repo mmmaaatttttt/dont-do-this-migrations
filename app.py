6fb8fd422c6f0c89af6d572de17b64d902588b79
Matt Lane <lane.matthew@gmail.com>
2018-03-12 14:22:48 -0700

    first migration

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://localhost/movies-db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    director = db.Column(db.Text)
